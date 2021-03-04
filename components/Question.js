ef9ec6085c9bd3b2e1ee9ad6020c13251918b398
SJellen <sjellen@me.com>
2021-03-04 15:12:21 -0500

    question container

--- components/Question.js
+++ components/Question.js
@@ -5,18 +5,32 @@ import styles from '../styles/Question.module.scss'
 
 export default function Question() {
 
-    const {currentQuestion} = useContext(Context)
+    const {currentQuestion, getShuffledArr} = useContext(Context)
+
+    const questionArr = currentQuestion && [...currentQuestion.incorrect_answers, currentQuestion.correct_answer]
+    const shuffledQuestions = questionArr && getShuffledArr(questionArr)
 
     
 
-    // console.log(currentQuestion && currentQuestion.question)
+    // console.log(questionArr, shuffledQuestions, currentQuestion.correct_answer)
 
 
     return (
         <div>
              { currentQuestion  ?
              <div className={styles.container}>
-            <h1>{currentQuestion && currentQuestion.question}</h1>
+                <div className={styles.questionContainer}>
+                    <h2>{currentQuestion && currentQuestion.question}</h2>
+                </div>
+            
+            <div className={styles.choiceContainer}>
+                <ul className={styles.list}>
+                    <li>{shuffledQuestions[0]}</li>
+                    <li>{shuffledQuestions[1]}</li>
+                    <li>{shuffledQuestions[2]}</li>
+                    <li>{shuffledQuestions[3]}</li>
+                </ul>
+            </div>
         </div> : ''
         }
         </div>
