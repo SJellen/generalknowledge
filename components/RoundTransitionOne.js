d2c975f4fe180f095ce386059b4294e986e85e7e
SJellen <sjellen@me.com>
2021-03-07 15:05:33 -0500

    work on round transitions

--- components/RoundTransitionOne.js
+++ components/RoundTransitionOne.js
@@ -1,15 +1,20 @@
 import React, {useContext, useEffect, useState} from 'react'
 import {GameContext} from '../context/GameContext'
+import styles from '../styles/RoundTransition.module.scss'
 
 
 export default function RoundTransitionOne() {
 
-    const {selectedQuestions} = useContext(GameContext)
+    const {selectedQuestions, score} = useContext(GameContext)
 
 
     return (
-        <div  style={{display: selectedQuestions === 30 ? "block" : "none"}}>
-            <h1>end of round one</h1>
+        <div  style={{display: selectedQuestions === 30 ? "block" : "none"}} className={styles.container}>
+            <div className={styles.textContainer}>
+                <h1>End of Round One.</h1>
+                <h2>Score: ${score}</h2>
+            </div>
+            
         </div>
     )
     
--- components/RoundTransitionTwo.js
+++ components/RoundTransitionTwo.js
@@ -1,15 +1,19 @@
 import React, {useContext, useEffect, useState} from 'react'
 import {GameContext} from '../context/GameContext'
-
+import styles from '../styles/RoundTransition.module.scss'
 
 export default function RoundTransitionTwo() {
 
-    const {selectedQuestions} = useContext(GameContext)
+    const {selectedQuestions, score} = useContext(GameContext)
 
 
     return (
-        <div  style={{display: selectedQuestions === 60 ? "block" : "none"}}>
-            <h1>end of round Two</h1>
+        <div  style={{display: selectedQuestions === 60 ? "block" : "none"}} className={styles.container}>
+            <div className={styles.textContainer}>
+                <h1>End of Round Two.</h1>
+                <h2>Score: ${score}</h2>
+            </div>
+            
         </div>
     )
     
