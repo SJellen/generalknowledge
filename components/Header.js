0be9580165a385169669f363a5e98a3a36c9d855
SJellen <sjellen@me.com>
2021-03-12 14:36:29 -0500

    small size changes

--- components/Header.js
+++ components/Header.js
@@ -6,15 +6,13 @@ import {GameContext} from '../context/GameContext'
 
 export default function Header() {
     
-    const {username, score, isStart, showInput} = useContext(GameContext)
+    const {username, score} = useContext(GameContext)
        
 
     return (
-        <div className={styles.container} 
-        // style={{display: isStart || showInput ? "none" : ''}}
-        >
+        <div className={styles.container} >
             <span className={styles.username}>{username && username}</span>
-            <span>${score ? score : 0}</span>
+            <span className={styles.score}>${score ? score : 0}</span>
             <span className={styles.title}>General Knowledge</span>
         </div>
     )
