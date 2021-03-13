79d1235416217f7c9838603914c78aef40fd18eb
SJellen <sjellen@me.com>
2021-03-13 14:36:31 -0500

    new logic hook for round one, re engineered the board to set up for cpu bot player

---
+++ logic/useRoundOneLogic.js
@@ -0,0 +1,99 @@
+import React, {useContext, useEffect, useState} from 'react'
+import {QuestionContext} from '../context/QuestionContext'
+import {GameContext} from '../context/GameContext'
+import Link from 'next/link'
+
+// localStorage.clear();
+const LOCAL_STORAGE_KEY_FR = 'firstround'
+
+
+export default function useRoundOneLogic() {
+
+    const {firstRoundQuestion1, firstRoundQuestion2, firstRoundQuestion3, firstRoundQuestion4, firstRoundQuestion5, firstRoundQuestion6, currentQuestion, setCurrentQuestion, setFirstRoundQuestion1, setFirstRoundQuestion2, setFirstRoundQuestion3, setFirstRoundQuestion4, setFirstRoundQuestion5, setFirstRoundQuestion6, fetchFirstRoundQuestions, getShuffledArr,setShuffledQuestionsArr} = useContext(QuestionContext)
+    const {setCost, setClockStart} = useContext(GameContext)
+    const [categoryArr, setCategoryArr] = useState()
+
+    
+   
+    function handleTileClick(e, question, questionCost, categoryIndex,index,) {
+        setCurrentQuestion(question)
+        setCost(questionCost)
+        categoryArr[categoryIndex][index] = ""
+        setCategoryArr([...categoryArr])
+        setClockStart(true)
+    }
+
+
+
+
+    useEffect(() => {
+        const questionArr = currentQuestion && [...currentQuestion.incorrect_answers, currentQuestion.correct_answer]
+        const shuffledQuestions = questionArr && getShuffledArr(questionArr)
+        setShuffledQuestionsArr(shuffledQuestions)
+        
+
+    }, [currentQuestion])
+   
+
+   
+
+
+    useEffect(() => {
+        const firstRoundCategories = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_FR))
+       
+        if (firstRoundCategories) {
+            if (firstRoundCategories[0]?.length !== 0) {
+                setFirstRoundQuestion1(firstRoundCategories[0])
+            } else {
+                fetchFirstRoundQuestions(roundOneCategories[0], setFirstRoundQuestion1)
+            }
+            if (firstRoundCategories[1]?.length !== 0) {
+                setFirstRoundQuestion2(firstRoundCategories[1])
+            } else {
+                fetchFirstRoundQuestions(roundOneCategories[1], setFirstRoundQuestion2)
+            }
+            if (firstRoundCategories[2]?.length !== 0) {
+                setFirstRoundQuestion3(firstRoundCategories[2])
+            } else {
+                fetchFirstRoundQuestions(roundOneCategories[2], setFirstRoundQuestion3)
+            }
+            if (firstRoundCategories[3]?.length !== 0) {
+                setFirstRoundQuestion4(firstRoundCategories[3])
+            } else {
+                fetchFirstRoundQuestions(roundOneCategories[3], setFirstRoundQuestion4)
+            }
+            if (firstRoundCategories[4]?.length !== 0) {
+                setFirstRoundQuestion5(firstRoundCategories[4])
+            } else {
+                fetchFirstRoundQuestions(roundOneCategories[4], setFirstRoundQuestion5) 
+            }
+            if (firstRoundCategories[4]?.length !== 0) {
+                 setFirstRoundQuestion6(firstRoundCategories[5])
+            } else {
+                fetchFirstRoundQuestions(roundOneCategories[5], setFirstRoundQuestion6)
+            }
+
+           
+        } else {
+            let categoryArr = [firstRoundQuestion1, firstRoundQuestion2, firstRoundQuestion3,firstRoundQuestion4,firstRoundQuestion5,firstRoundQuestion6]
+            localStorage.setItem(LOCAL_STORAGE_KEY_FR, JSON.stringify(categoryArr))
+        }
+
+    }, [])
+
+
+   
+
+
+    useEffect(() => {
+        setCategoryArr([firstRoundQuestion1, firstRoundQuestion2, firstRoundQuestion3, firstRoundQuestion4, firstRoundQuestion5, firstRoundQuestion6])
+        
+    }, [firstRoundQuestion1, firstRoundQuestion2, firstRoundQuestion3, firstRoundQuestion4, firstRoundQuestion5, firstRoundQuestion6])
+
+
+
+
+
+
+    return {categoryArr,handleTileClick}
+}
--- pages/roundone.js
+++ pages/roundone.js
@@ -3,104 +3,60 @@ import {QuestionContext} from '../context/QuestionContext'
 import {GameContext} from '../context/GameContext'
 import Link from 'next/link'
 import styles from '../styles/RoundOne.module.scss'
+import useRoundOneLogic from '../logic/useRoundOneLogic'
 
 
 // localStorage.clear();
-const LOCAL_STORAGE_KEY_FR = 'firstround'
+// const LOCAL_STORAGE_KEY_FR = 'firstround'
 
 export default function RoundOne() {
 
-    const {firstRoundQuestion1, firstRoundQuestion2, firstRoundQuestion3, firstRoundQuestion4, firstRoundQuestion5, firstRoundQuestion6, categoryCleaner, currentQuestion, setCurrentQuestion, setFirstRoundQuestion1, setFirstRoundQuestion2, setFirstRoundQuestion3, setFirstRoundQuestion4, setFirstRoundQuestion5, setFirstRoundQuestion6, fetchFirstRoundQuestions, getShuffledArr,setShuffledQuestionsArr} = useContext(QuestionContext)
-    const {selectedQuestions,setCost, setClockStart} = useContext(GameContext)
-    const [categoryArr, setCategoryArr] = useState()
+    const {categoryArr, handleTileClick} = useRoundOneLogic()
+
+    const {categoryCleaner, currentQuestion} = useContext(QuestionContext)
+    const {selectedQuestions} = useContext(GameContext)
+
+    useEffect(() => {
+
+    }, [categoryArr])
+
 
     
-   
-    function handleTileClick(e, question, questionCost, id) {
-        setCurrentQuestion(question)
-        
-        setCost(questionCost)
-        let x = document.getElementById(id)
-        x.innerHTML = ""
-        x.style.pointerEvents = "none"
-        setClockStart(true)
-        
-    }
-
-
-    useEffect(() => {
-        const questionArr = currentQuestion && [...currentQuestion.incorrect_answers, currentQuestion.correct_answer]
-        const shuffledQuestions = questionArr && getShuffledArr(questionArr)
-        setShuffledQuestionsArr(shuffledQuestions)
-        
-
-    }, [currentQuestion])
-   
-
-   
-
-
-    useEffect(() => {
-        const firstRoundCategories = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_FR))
-       
-        if (firstRoundCategories) {
-            if (firstRoundCategories[0]?.length !== 0) {
-                setFirstRoundQuestion1(firstRoundCategories[0])
-            } else {
-                fetchFirstRoundQuestions(roundOneCategories[0], setFirstRoundQuestion1)
-            }
-            if (firstRoundCategories[1]?.length !== 0) {
-                setFirstRoundQuestion2(firstRoundCategories[1])
-            } else {
-                fetchFirstRoundQuestions(roundOneCategories[1], setFirstRoundQuestion2)
-            }
-            if (firstRoundCategories[2]?.length !== 0) {
-                setFirstRoundQuestion3(firstRoundCategories[2])
-            } else {
-                fetchFirstRoundQuestions(roundOneCategories[2], setFirstRoundQuestion3)
-            }
-            if (firstRoundCategories[3]?.length !== 0) {
-                setFirstRoundQuestion4(firstRoundCategories[3])
-            } else {
-                fetchFirstRoundQuestions(roundOneCategories[3], setFirstRoundQuestion4)
-            }
-            if (firstRoundCategories[4]?.length !== 0) {
-                setFirstRoundQuestion5(firstRoundCategories[4])
-            } else {
-                fetchFirstRoundQuestions(roundOneCategories[4], setFirstRoundQuestion5) 
-            }
-            if (firstRoundCategories[4]?.length !== 0) {
-                 setFirstRoundQuestion6(firstRoundCategories[5])
-            } else {
-                fetchFirstRoundQuestions(roundOneCategories[5], setFirstRoundQuestion6)
-            }
-
-           
-        } else {
-            let categoryArr = [firstRoundQuestion1, firstRoundQuestion2, firstRoundQuestion3,firstRoundQuestion4,firstRoundQuestion5,firstRoundQuestion6]
-            localStorage.setItem(LOCAL_STORAGE_KEY_FR, JSON.stringify(categoryArr))
-        }
-
-    }, [])
-
-
-   
-
-
-    useEffect(() => {
-        setCategoryArr([firstRoundQuestion1, firstRoundQuestion2, firstRoundQuestion3, firstRoundQuestion4, firstRoundQuestion5, firstRoundQuestion6])
-        
-    }, [firstRoundQuestion1, firstRoundQuestion2, firstRoundQuestion3, firstRoundQuestion4, firstRoundQuestion5, firstRoundQuestion6])
 
 
     const categories = categoryArr && categoryArr.map((cat, index) => (
         <div key={index}>
             <div className={styles.gridTitle}>{cat && categoryCleaner(cat[0]?.category)}</div>
-            <div className={styles.gridItem} id={index} onClick={(e) => handleTileClick(e, cat[0], 200, index)}>$200</div>
-            <div className={styles.gridItem} id={index} onClick={(e) => handleTileClick(e, cat[1], 400, index)}>$400</div>
-            <div className={styles.gridItem} id={index} onClick={(e) => handleTileClick(e, cat[2], 600, index)}>$600</div>
-            <div className={styles.gridItem} id={index} onClick={(e) => handleTileClick(e, cat[3], 800, index)}>$800</div>
-            <div className={styles.gridItem} id={index} onClick={(e) => handleTileClick(e, cat[4], 1000, index)}>$1000</div>
+            <div 
+                className={styles.gridItem} 
+                id={index}
+                style={{pointerEvents: cat && cat[1] === '' ? "none" : '' }} 
+                onClick={(e) => handleTileClick(e, cat[1], 200, index, 1)}>{cat && cat[1] !== '' ? "$200" : ''}
+            </div>
+            <div 
+                className={styles.gridItem} 
+                id={index}
+                style={{pointerEvents: cat && cat[2] === '' ? "none" : '' }} 
+                onClick={(e) => handleTileClick(e, cat[2], 400, index, 2)}>{cat && cat[2] !== '' ? "$400" : ''}
+            </div>
+            <div 
+                className={styles.gridItem} 
+                id={index}
+                style={{pointerEvents: cat && cat[3] === '' ? "none" : '' }} 
+                onClick={(e) => handleTileClick(e, cat[3], 600, index, 3)}>{cat && cat[3] !== '' ? "$600" : ''}
+            </div>
+            <div 
+                className={styles.gridItem} 
+                id={index}
+                style={{pointerEvents: cat && cat[4] === '' ? "none" : '' }} 
+                onClick={(e) => handleTileClick(e, cat[4], 800, index, 4)}>{cat && cat[4] !== '' ? "$800" : ''}
+            </div>
+            <div 
+                className={styles.gridItem} 
+                id={index}
+                style={{pointerEvents: cat && cat[5] === '' ? "none" : '' }} 
+                onClick={(e) => handleTileClick(e, cat[5], 1000, index, 5)}>{cat && cat[5] !== '' ? "$1000" : ''}
+            </div>
         </div>
     ))
 
