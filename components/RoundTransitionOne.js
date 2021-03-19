d2affab41143194aef16442dbd8772d4bcdecf00
SJellen <sjellen@me.com>
2021-03-19 13:46:58 -0400

    added all scores to round transition

--- components/RoundTransitionOne.js
+++ components/RoundTransitionOne.js
@@ -8,7 +8,7 @@ import Link from 'next/link'
 
 export default function RoundTransitionOne() {
 
-    const {selectedQuestions, score, isRoundTwo, setIsRoundTwo} = useContext(GameContext)
+    const {selectedQuestions, score, player2Score, player3Score, username,player2, player3,  isRoundTwo, setIsRoundTwo} = useContext(GameContext)
 
     function handleRoundTwoClick() {
         setIsRoundTwo(true)
@@ -20,7 +20,12 @@ export default function RoundTransitionOne() {
         <div  style={{display: selectedQuestions === 30 && !isRoundTwo && score >= 0 ? "block": "none"}} className={styles.container}>
             <div className={styles.textContainer}>
                 <h1>End of Round One.</h1>
-                <h2>Score: ${score}</h2>
+                <h2>{username}: ${score}</h2>
+                <h2>{player2}: ${player2Score}</h2>
+                <h2>{player3}: ${player3Score}</h2>
+
+
+
                 <div className={styles.roundTwoLink}><Link href="/roundtwo" style={{textDecoration: "none"}}><a className={styles.beginButton} style={{color: "black"}}
                 onClick={handleRoundTwoClick}
                 >Round Two</a></Link></div>
--- components/RoundTransitionTwo.js
+++ components/RoundTransitionTwo.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export default function RoundTransitionTwo() {
 
-    const {selectedQuestions, score, isRoundTwo, setIsRoundTwo, isRoundThree, setIsRoundThree} = useContext(GameContext)
+    const {selectedQuestions, score, isRoundTwo,player2Score, player3Score, username,player2, player3, setIsRoundTwo, isRoundThree, setIsRoundThree} = useContext(GameContext)
 
 
     function handleRoundTwoClick() {
@@ -19,7 +19,9 @@ export default function RoundTransitionTwo() {
         <div  style={{display: selectedQuestions === 60 && !isRoundThree && score >= 0 ? "block" : "none"}} className={styles.container}>
             <div className={styles.textContainer}>
                 <h1>End of Round Two.</h1>
-                <h2>Score: ${score}</h2>
+                <h2>{username}: ${score}</h2>
+                <h2>{player2}: ${player2Score}</h2>
+                <h2>{player3}: ${player3Score}</h2>
                 <div className={styles.roundTwoLink}><Link href="/finalround" style={{textDecoration: "none"}}><a className={styles.beginButton} style={{color: "black"}}
                 onClick={handleRoundTwoClick}
                 >Final Round</a></Link></div>
