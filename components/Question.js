0063860fc49d3bee3ed5f4a22502d59a7db069d1
SJellen <sjellen@me.com>
2021-03-23 13:36:19 -0400

    small edits

--- components/Question.js
+++ components/Question.js
@@ -8,7 +8,7 @@ import useQuestionLogic from '../logic/useQuestionLogic'
 export default function Question() {
 
     const {currentQuestion, questionCleaner, shuffledQuestionsArr} = useContext(QuestionContext)
-    const {timeRemaining, username, currentTurn, showButtons, player2Move, setPlayer2Move, player3Move, setPlayer3Move, player2, player3} = useContext(GameContext)
+    const {timeRemaining, username, currentTurn, showButtons, player2Move, setPlayer2Move, player3Move, setPlayer3Move, player2, player3, userMove} = useContext(GameContext)
 
     const {handleClick, handlePassClick, handlePlayClick} = useQuestionLogic()
 
@@ -18,7 +18,7 @@ export default function Question() {
         <div>
              { currentQuestion  ?
              <div className={styles.container}>
-                <div className={styles.buttonContainer}>
+                <div className={styles.buttonContainer} style={{visibility: userMove === "Pass" ? "visible" : 'hidden'}}>
                     <div className={styles.passContainer} style={{visibility: showButtons ? "visible" : 'hidden'}}>
                         <button className={styles.passButton} onClick={handlePassClick}>Pass</button>
                     </div>
@@ -27,13 +27,13 @@ export default function Question() {
                     </div>
                
                 </div>
-                 <div className={styles.questionContainer}>
+                 <div className={styles.questionContainer} >
                     <h2>{currentQuestion && questionCleaner(currentQuestion.question)}</h2>
                     <span className={styles.timer} style={{visibility: timeRemaining <= 5 ? "visible" : 'hidden'}}>{timeRemaining}</span>
                 </div>
             
             <div className={styles.choiceContainer} style={{display: !showButtons && currentTurn === username ? "block" : 'none'}}>
-                <ul className={styles.list}>
+                <ul className={styles.list} >
                     <li onClick={(e) => handleClick(questionCleaner(shuffledQuestionsArr && shuffledQuestionsArr[0]))}>{questionCleaner(shuffledQuestionsArr && shuffledQuestionsArr[0])}</li>
                     <li onClick={(e) => handleClick(questionCleaner(shuffledQuestionsArr && shuffledQuestionsArr[1]))}>{questionCleaner(shuffledQuestionsArr && shuffledQuestionsArr[1])}</li>
                     <li onClick={(e) => handleClick(questionCleaner(shuffledQuestionsArr && shuffledQuestionsArr[2]))}>{questionCleaner(shuffledQuestionsArr && shuffledQuestionsArr[2])}</li>
