623a2a6abbaa1dcb85490d4ea83cdab1c9193996
SJellen <sjellen@me.com>
2021-03-28 13:05:11 -0400

    code adjustment

--- components/FinalQuestion.js
+++ components/FinalQuestion.js
@@ -9,87 +9,17 @@ import useFinalRoundTransitionLogic from '../logic/useFinalRoundTransitionLogic'
 
 export default function FinalQuestion() {
      
-    const {selectedQuestions, score, isRoundTwo,player2Score, player3Score, username,player2, player3, setIsRoundTwo, isRoundThree, setIsRoundThree, userWager, setUserWager, player2Wager, setPlayer2Wager, player3Wager, setPlayer3Wager, timeRemaining, setSelectedQuestions, setScore, setAnswerResult, setUserMove, setPlayer2Score, setPlayer3Score, setTimeRemaining, clockStart, setClockStart, START_TIME, player2Move, setPlayer2Move, player3Move, setPlayer3Move,userMove, } = useContext(GameContext)
-    const {finalQuestion, setCurrentQuestion, questionCleaner, shuffledQuestionsArr, setShuffledQuestionsArr, getShuffledArr} = useContext(QuestionContext)
+    const {selectedQuestions, timeRemaining } = useContext(GameContext)
+    const {finalQuestion, questionCleaner, shuffledQuestionsArr} = useContext(QuestionContext)
 
 
-    const {} = useFinalQuestionLogic()
-
-    console.log(typeof userWager)
-
-
-    function computerAnswersQuestion() {
-        let move = Math.floor(Math.random() * 1000)
-        return move % 2 === 0 || move % 5 === 0  ? "Correct" : "Incorrect"
-        
-    }
-
-    function computerPlayersAnswers() {
-        let player2Answer = computerAnswersQuestion()
-        let player3Answer = computerAnswersQuestion()
-        setTimeout(() => {
-            if (player2Answer === "Correct") {
-                        setPlayer2Score(prevScore => prevScore + player2Wager)
-                        setPlayer2Move("Correct")
-                    } else {
-                        setPlayer2Score(prevScore => prevScore - player2Wager)
-                        setPlayer2Move("Incorrect")
-                    }
-        }, 750)
-        
-
-        setTimeout(() => {
-            if (player3Answer === "Correct") {
-                        setPlayer3Score(prevScore => prevScore + player2Wager)
-                        setPlayer3Move("Correct")
-                    } else {
-                        setPlayer3Score(prevScore => prevScore - player2Wager)
-                        setPlayer3Move("Incorrect")
-                    }
-        }, 1500)
-        
-    }
-
-
-    
-
-    function handleClick(choice) {
-        setSelectedQuestions(prevState => prevState + 1)
-        computerPlayersAnswers()
-        if (choice === finalQuestion[0].correct_answer) {
-            setUserMove("Correct")
-            setScore(prevScore => prevScore + userWager)
-            // setAnswerResult("Correct")
-            // // moveResetter()
-            // setTimeout(() => {
-            //     setAnswerResult()
-                 
-            // }, 750)    
-        } else {
-            setUserMove("InCorrect")
-            setScore(prevScore => prevScore - userWager)
-            setAnswerResult("Incorrect")
-            setUserMove("Incorrect")
-            // setTimeout(() => {
-            //     setAnswerResult()
-            // }, 750)
-            
-        }
-
-        setClockStart(false)
-        setTimeRemaining(START_TIME)
-    }
-    
-
-    
+    const {handleClick} = useFinalQuestionLogic()
 
 
     return (
         <div>
              <div className={styles.container} style={{display: selectedQuestions === 62 ? "block" : "none"}}>
 
-              
-               
                  <div className={styles.questionContainer} >
                     <h2>{finalQuestion && questionCleaner(finalQuestion[0].question)}</h2>
                     <span className={styles.timer} style={{visibility: timeRemaining <= 5 ? "visible" : 'hidden'}}>{timeRemaining}</span>
--- logic/useFinalQuestionLogic.js
+++ logic/useFinalQuestionLogic.js
@@ -8,17 +8,60 @@ export default function useFinalQuestionLogic() {
 
 
     const {finalQuestion, currentQuestion, setCurrentQuestion} = useContext(QuestionContext)
-    const {setCost, currentTurn, username,  setShowButtons, selectedQuestions, setSelectedQuestions} = useContext(GameContext)
+    const {selectedQuestions, score, isRoundTwo,player2Score, player3Score, username,player2, player3, setIsRoundTwo, isRoundThree, setIsRoundThree, userWager, setUserWager, player2Wager, setPlayer2Wager, player3Wager, setPlayer3Wager, timeRemaining, setSelectedQuestions, setScore, setAnswerResult, setUserMove, setPlayer2Score, setPlayer3Score, setTimeRemaining, clockStart, setClockStart, START_TIME, player2Move, setPlayer2Move, player3Move, setPlayer3Move,userMove,} = useContext(GameContext)
 
 
-    console.log(finalQuestion)
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setCurrentQuestion(finalQuestion)
-    //     }, 2500)
-    // }, [])
+    function computerAnswersQuestion() {
+        let move = Math.floor(Math.random() * 1000)
+        return move % 2 === 0 || move % 5 === 0  ? "Correct" : "Incorrect"
+        
+    }
+
+    function computerPlayersAnswers() {
+        let player2Answer = computerAnswersQuestion()
+        let player3Answer = computerAnswersQuestion()
+        setTimeout(() => {
+            if (player2Answer === "Correct") {
+                        setPlayer2Score(prevScore => prevScore + player2Wager)
+                        setPlayer2Move("Correct")
+                    } else {
+                        setPlayer2Score(prevScore => prevScore - player2Wager)
+                        setPlayer2Move("Incorrect")
+                    }
+        }, 750)
+        
+
+        setTimeout(() => {
+            if (player3Answer === "Correct") {
+                        setPlayer3Score(prevScore => prevScore + player3Wager)
+                        setPlayer3Move("Correct")
+                    } else {
+                        setPlayer3Score(prevScore => prevScore - player3Wager)
+                        setPlayer3Move("Incorrect")
+                    }
+        }, 1500)
+        
+    }
+
+
+    function handleClick(choice) {
+        setSelectedQuestions(prevState => prevState + 1)
+        computerPlayersAnswers()
+        if (choice === finalQuestion[0].correct_answer) {
+            setUserMove("Correct")
+            setScore(prevScore => prevScore + userWager)   
+        } else {
+            setUserMove("InCorrect")
+            setScore(prevScore => prevScore - userWager)
+            setAnswerResult("Incorrect")
+            setUserMove("Incorrect")
+        }
+
+        setClockStart(false)
+        setTimeRemaining(START_TIME)
+    }
     
     
-    return {}
+    return {handleClick}
 
 }
