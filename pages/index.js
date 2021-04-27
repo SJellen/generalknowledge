0c0ed277064e5d53671ecb7329a8ecac5a7c5f8a
SJellen <sjellen@me.com>
2021-04-27 13:54:23 -0400

    work on the background color

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,17 @@
-import React, {useContext, useEffect, useState} from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+import React from 'react'
 import Start from '../components/Start'
-import {GameContext} from '../context/GameContext'
 import RoundOne from '../components/Roundone'
 import RoundTwo from '../components/RoundTwo'
 import FinalRound from '../components/FinalRound'
 
 export default function Home() {
 
-  const {selectedQuestions, currentTurn, username, setCurrentTurn, isRoundTwo, isRoundOne, setIsRoundTwo} = useContext(GameContext)
-
- 
-
-  
   return (
-    <div className={styles.container} >
-    
-     <Start />
-     <RoundOne />
-     <RoundTwo />
-     <FinalRound />
-    
+    <div >
+      <Start />
+      <RoundOne />
+      <RoundTwo />
+      <FinalRound />
     </div>
   )
 }
