be690d4fcb129da19da1ccf93976a82f93a1b8bc
SJellen <sjellen@me.com>
2021-08-03 16:12:08 -0400

    edit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Start from '../components/Start'
-
 import RoundOne from '../components/RoundOne/RoundOne'
 import RoundTwo from '../components/RoundTwo/RoundTwo'
 import FinalRound from '../components/FinalRound/FinalRound'
